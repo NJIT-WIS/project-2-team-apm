944c7e95c9a1cf57262c88587b59eac7bfd1f8bc
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-04-11 02:06:17 -0400

    playwright last testing

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Team APM';
-export const siteTitle = 'APM-MyWebClass';
+const name = 'MyWebClass';
+export const siteTitle = 'MyWebClass.org';
 
 export default function Layout({ children, home }) {
   return (
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile1.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +43,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/profile1.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,18 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
 export default function About() {
-  return <div>About
-    <p>
-    <h> </h>
-      </p>
-  </div>
+  return (
+    <>
+      <Head>
+        <title>MyWebClass.org | About</title>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <h2>
+          <Link href="/">← Back to home</Link>
+        </h2>
+      </div>
+    </>
+  );
 }
