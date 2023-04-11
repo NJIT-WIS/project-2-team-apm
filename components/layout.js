82c04e0bc7af14c297d050399025896a64c5ea22
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-04-11 01:37:46 -0400

    fixing playwright

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Team APM';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'APM-MyWebClass';
 
 export default function Layout({ children, home }) {
   return (
