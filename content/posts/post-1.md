c57d8b24e909c96a970cea8b5683e6e2878e24f0
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-04-21 13:34:42 -0400

    front page picture and new favicon

--- content/posts/post-1.md
+++ content/posts/post-1.md
@@ -1,11 +1,11 @@
 ---
 title: "Welcome to MyWebClass"
 description: "meta description"
-date: 2022-04-04T05:00:00Z
-image: "images/posts/01.jpg"
-categories: ["art"]
+date: 2023-04-21T05:00:00Z
+image: "images/posts/post1.jpg"
+categories: ["education"]
 authors: ["Mark Dinn"]
-tags: ["diy", "toy"]
+tags: ["teaching", "ai"]
 draft: false
 ---
 With MyWebClass, you can gain access to a wealth of knowledge and resources that will help you stay up-to-date with the latest advancements in software engineering.
--- pages/_document.js
+++ pages/_document.js
@@ -13,18 +13,6 @@ const Document = () => {
         {/* favicon */}
         <link rel="shortcut icon" href="images/favicon.ico" />
 
-        {/* Open Graph tags */}
-        <meta property="og:title" content="My page title" />
-        <meta property="og:description" content="My page description" />
-        <meta property="og:image" content="https://example.com/my-image.jpg" />
-        <meta property="og:url" content="https://example.com/my-page" />
-        {/* Twitter tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="My page title" />
-        <meta name="twitter:description" content="My page description" />
-        <meta name="twitter:image" content="https://example.com/my-image.jpg" />
-        <meta name="twitter:url" content="https://example.com/my-page" />
-
         <div>
         {/* theme meta */}
         <meta name="theme-name" content="bookworm-light-nextjs" />
