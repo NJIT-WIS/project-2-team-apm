ffc004ce903d510ca257de58d5d7e02aa5e7f506
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-04-24 14:02:34 -0400

    Google Tag

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import TwSizeIndicator from "@components/TwSizeIndicator";
 import config from "@config/config.json";
 import { Head, Html, Main, NextScript } from "next/document";
 import Script from 'next/script'
-
+import Document, { Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   // destructuring items from config object
@@ -28,6 +28,17 @@ const Document = () => {
           content="#000"
         />
         </div>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KNDDNWP');
+              `,
+            }}
+          />
     <div className="container">
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-W7885XHR5D"></script>
           <script
@@ -45,6 +56,20 @@ const Document = () => {
     </div>
       </Head>
       <body>
+
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe
+                  src="https://www.googletagmanager.com/ns.html?id=GTM-KNDDNWP"
+                  height="0"
+                  width="0"
+                  style="display:none;visibility:hidden"
+                ></iframe>
+              `,
+            }}
+          />
+
         <Main />
         <TwSizeIndicator />
         <NextScript />
