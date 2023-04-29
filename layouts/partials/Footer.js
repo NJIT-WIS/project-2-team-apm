c8d63e06e342c69f250148465c90ffe0e10deed4
Asingh027 <avneetsingh.007@gmail.com>
2023-04-29 02:26:07 -0400

    Fixed footer by re-adding old content while keeping new integration.

--- layouts/partials/Footer.js
+++ layouts/partials/Footer.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import Social from "@components/Social";
+import config from "@config/config.json";
+import menu from "@config/menu.json";
+import social from "@config/social.json";
+import { markdownify } from "@lib/utils/textConverter";
+import Link from "next/link";
 
 const Footer = () => {
+  const { copyright } = config.params;
   const [email, setEmail] = useState("");
 
   const handleFormSubmit = (event) => {
@@ -17,15 +24,31 @@ const Footer = () => {
 
   return (
     <footer className="section bg-theme-dark">
-      <div className="container flex flex-wrap items-center justify-between text-center md:text-left">
+      <div className="container flex flex-col md:flex-row items-center md:justify-between text-center md:text-left">
+        {/* footer menu */}
+        <ul className="md:w-2/3 space-x-4">
+          {menu.footer.map((menu) => (
+            <li className="inline-block" key={menu.name}>
+              <Link href={menu.url} className="p-4 text-light hover:text-white">
+                {menu.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
         {/* email submission */}
-        <form onSubmit={handleFormSubmit} method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="mb-8 md:w-1/3 md:mb-0">
+        <form onSubmit={handleFormSubmit} method="post" className="mb-8 md:w-1/4">
           <label htmlFor="email" className="block mb-2 text-light font-bold">Subscribe to our newsletter:</label>
           <div className="flex items-center">
             <input type="email" id="email" name="EMAIL" placeholder="Your email address" className="rounded-l-lg px-4 py-2 flex-1 bg-white" value={email} onChange={handleInputChange} required />
             <button type="submit" className="rounded-r-lg px-4 py-2 bg-theme text-light hover:bg-theme-light transition-colors duration-300">Subscribe</button>
           </div>
         </form>
+      </div>
+      {/* copyright */}
+      <div className="text-center mt-8 md:mt-0">
+        {/* social icons */}
+        <Social source={social} className="social-icons mb-8 md:mb-0" />
+        {markdownify(copyright, "p", "text-light")}
       </div>
     </footer>
   );
