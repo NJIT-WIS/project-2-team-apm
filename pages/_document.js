e6e41b87a9706e23838ca46bac1b46d87e2ef0dc
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-04-29 16:38:58 -0400

    Google Tag another attempt

--- pages/_document.js
+++ pages/_document.js
@@ -9,33 +9,6 @@ const CustomDocument = () => {
   return (
     <Html lang="en">
       <DocumentHead>
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        gtag('consent', 'default', {
-          'ad_storage': 'denied',
-          'analytics_storage': 'denied',
-          'personalization_storage': 'denied'
-        });
-        gtag('set', 'ads_data_redaction', true);
-      }
-    `
-  }}
-/>
-
-{/* Google Tag Manager */}
-<script
-  dangerouslySetInnerHTML={{
-    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MFP2RZL');`,
-  }}
-/>
-{/* End Google Tag Manager */}
 
         {/* favicon */}
         <link rel="shortcut icon" href="images/favicon.ico" />
