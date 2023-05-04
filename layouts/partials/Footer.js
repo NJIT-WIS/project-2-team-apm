abc783d7a573e1e6643aba75da86f62e040b3eca
Pil Jang <123077642+PGJ94@users.noreply.github.com>
2023-05-03 21:38:27 -0400

    Fix

--- layouts/partials/Footer.js
+++ layouts/partials/Footer.js
@@ -12,7 +12,6 @@ const Footer = () => {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-<<<<<<< Updated upstream
     const form = event.target;
     form.action = `https://njit.us21.list-manage.com/subscribe/post?u=7d11727fe19a05ff0c992a7d8&amp;id=ee9ffaca2f&amp;f_id=0037ade1f0&EMAIL=${email}`;
     form.submit();
@@ -21,14 +20,6 @@ const Footer = () => {
 
   const handleInputChange = (event) => {
     setEmail(event.target.value);
-=======
-    const emailAddress = event.target.email.value;
-    // addSubscriber function not needed anymore
-    // email form will be submitted to Mailchimp directly
-    event.target.submit();
-    alert('Thank you for subscribing to our newsletter!');
-    event.target.reset();
->>>>>>> Stashed changes
   }
 
   return (
@@ -45,7 +36,6 @@ const Footer = () => {
           ))}
         </ul>
         {/* email submission */}
-<<<<<<< Updated upstream
         <form onSubmit={handleFormSubmit} method="post" className="mb-8 md:w-1/4">
           <label htmlFor="email" className="block mb-2 text-light font-bold">Subscribe to our newsletter:</label>
           <div className="flex items-center">
@@ -53,26 +43,6 @@ const Footer = () => {
             <button type="submit" className="rounded-r-lg px-4 py-2 bg-theme text-light hover:bg-theme-light transition-colors duration-300">Subscribe</button>
           </div>
         </form>
-=======
-        <div id="mc_embed_signup">
-          <form action="https://njit.us21.list-manage.com/subscribe/post?u=7d11727fe19a05ff0c992a7d8&amp;id=ee9ffaca2f&amp;f_id=0037ade1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate onSubmit={handleFormSubmit}>
-            <div id="mc_embed_signup_scroll">
-              <h2>Subscribe</h2>
-              <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-              <div class="mc-field-group">
-                <label htmlFor="mce-EMAIL">Email Address <span class="asterisk">*</span></label>
-                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required onChange={() => {}} />
-                <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-              </div>
-              <div id="mce-responses" class="clear foot">
-                <div class="response" id="mce-error-response" style={{ display: "none" }}></div>
-                <div class="response" id="mce-success-response" style={{ display: "none" }}></div>
-
-              </div>
-            </div>
-          </form>
-        </div>
->>>>>>> Stashed changes
       </div>
       {/* copyright */}
       <div className="text-center mt-8 md:mt-0">
